063616e43c88eb1df39eeaaf7e03e5f57b7166a2
Darvi <hunter18111982@gmail.com>
2022-08-02 19:57:39 +0300

    hw_2

--- src/HW_2/Main.java
+++ src/HW_2/Main.java
@@ -1,16 +1,18 @@
 package HW_2;
 
+import java.util.Locale;
+
 public class Main {
     static int wordsCount(String a) {
         return a.split(" ").length;
     }
     static boolean isPalindrome(String a) {
         StringBuilder b = new StringBuilder(a);
-        return b.reverse().toString().equals(a);
+        return b.reverse().toString().equalsIgnoreCase(a);
     }
     public static void main(String[] args) {
         String a = "Glory to Ukraine!";
-        String b = "lol";
+        String b = "loLoL";
         System.out.println(wordsCount(a));
         System.out.println(isPalindrome(b));
     }
