9d493917e009836328027d41b39f508b2ae31be0
Darvi <hunter18111982@gmail.com>
2022-10-18 20:22:50 +0300

    hw_9

---
+++ src/HW_9/Main.java
@@ -0,0 +1,8 @@
+package HW_9;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        ValueCalculator valueCalculator = new ValueCalculator();
+        valueCalculator.doCalc();
+    }
+}
